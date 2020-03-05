4d229ea0175bd9339533cc48c9236c176d1e98f0
loowa103118 <56136552+loowa103118@users.noreply.github.com>
2020-03-05 12:53:42 -0800

    Update urls.py

--- pythonclub/clubapp/urls.py
+++ pythonclub/clubapp/urls.py
@@ -1,8 +1,11 @@
 
 
+
 from django.urls import path
 from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('getmeetings/', views.getmeetings, name='meeting'),
+    
 ]
