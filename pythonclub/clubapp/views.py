d1eecf4a8602e6edabb568393249d3370a28ca45
loowa103118 <56136552+loowa103118@users.noreply.github.com>
2020-03-05 12:56:18 -0800

    Update views.py

--- pythonclub/clubapp/views.py
+++ pythonclub/clubapp/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Meeting
 
 
 
 # Create your views here.
 def index (request):
     return render(request, 'clubapp/index.html')
+
+def getmeetings(request):
+     type_list=Meeting.objects.all() 
+     return render(request, 'clubapp/meeting.html', {'type_list': type_list})   
+
+    
